d57f63f766a24d333c7901b3f272afd5149d5f8b
van <vmyers@olin.edu>
2021-09-29 15:41:54 -0400

    3-8 decoder and tested

--- 01_game_of_life/decoder_2_to_4.sv
+++ 01_game_of_life/decoder_2_to_4.sv
@@ -4,5 +4,10 @@ module decoder_2_to_4(ena, in, out);
   input wire [1:0] in;
   output logic [3:0] out;
 
+  logic [1:0] ena1;
+  decoder_1_to_2 switcher(ena,in[1],ena1);
+  decoder_1_to_2 dec1(ena1[1],in[0],out[3:2]);
+  decoder_1_to_2 dec0(ena1[0],in[0],out[1:0]);
+
 
 endmodule
