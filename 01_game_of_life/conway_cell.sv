1c4ed146f834a5de26ad745aa2508f6cd1e1678d
van <vmyers@olin.edu>
2021-10-04 18:11:57 -0400

    conway cell tested

--- 01_game_of_life/conway_cell.sv
+++ 01_game_of_life/conway_cell.sv
@@ -10,17 +10,21 @@ module conway_cell(clk, rst, ena, state_0, state_d, state_q, neighbors);
   output logic state_q;
 
   input wire [7:0] neighbors;
-  logic [3:0] living_neighbors; // Lonely, 2, 3, overpopulated
+  logic [3:0] living_neighbors;
 
 // Count living neighbors
+bit_counter living(neighbors, living_neighbors);
 
+// The Conway's game logic
+logic stay_alive;
 
-
-//
-always_comb begin : next_state
-
-  
+always_comb begin
+  stay_alive = state_q & (living_neighbors == 4'b0010);
+  state_d = stay_alive | (living_neighbors == 4'b0011);
 end
 
+always_ff @(posedge clk) begin
+  state_q <= ena? (rst? state_0 : state_d) : state_q;
+end
 
 endmodule
