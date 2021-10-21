8a10413711ab9d4316929a3711b529e9f66904cd
van <vmyers@olin.edu>
2021-10-21 16:17:20 -0400

    muxn

--- hw_04/muxn.sv
+++ hw_04/muxn.sv
@@ -7,10 +7,10 @@ input wire [$clog2(N):0] sel;
 input wire [N*WIDTH-1:0] inputs;
 output wire [WIDTH-1:0] out;
 
-wire [WIDTH-1:0] top_mux,bottom_mux;
+wire [WIDTH-1:0] top_out,bot_out;
 generate
   genvar i;
   for(i = 0; i < N; i++) begin : tree_mux
-    mux2 
+    mux2 top_mux(.s(s[i]),.a(inputs),.b,.out)
 
 endmodule
