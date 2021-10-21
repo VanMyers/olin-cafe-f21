1a7526c82e2df1609cd192a8e44a34fde3b5a3da
van <vmyers@olin.edu>
2021-10-21 17:35:43 -0400

    pulse generator complete and working

--- 02_etch_a_sketch/pulse_generator.sv
+++ 02_etch_a_sketch/pulse_generator.sv
@@ -11,5 +11,15 @@ output logic out;
 
 logic [N-1:0] counter;
 logic counter_comparator;
+logic counter_rst;
+
+counter #(.N(N)) count(.clk(clk),.rst(counter_rst),.ena(ena),.out(counter));
+
+always_comb begin : comparator
+  counter_comparator = (counter == ticks-1);
+  counter_rst = counter_comparator | rst;
+  out = counter_comparator & ena;
+end
+
 
 endmodule
