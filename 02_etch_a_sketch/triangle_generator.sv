59632f69b1710144392b03eee1c65c5cea30b064
van <vmyers@olin.edu>
2021-10-25 02:49:32 -0400

    I gave up on using the carry bit

--- 02_etch_a_sketch/triangle_generator.sv
+++ 02_etch_a_sketch/triangle_generator.sv
@@ -8,5 +8,30 @@ output logic [N-1:0] out;
 
 typedef enum logic {COUNTING_UP, COUNTING_DOWN} state_t;
 state_t state;
+logic carry;
+
+// I really wanted to make using carry as the signal work but it was a cycle too slow
+always_ff @(posedge clk) begin
+  if (rst) begin
+    state <= 0;
+  end
+  else begin
+    if (ena) begin
+      if (d == 0) begin
+        state <= 1;
+      end
+      else begin
+        if (d == 2**(N)-1) begin
+        state <= 0;
+        end
+      end
+    end
+  end
+end
+
+logic [N-1:0] d;
+always_comb d = state ? out+1 : out-1;
+
+register #(.N(N)) value(.clk(clk),.ena(ena),.rst(rst),.d(d),.q(out));
 
 endmodule
