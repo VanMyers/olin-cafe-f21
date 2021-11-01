97dd606043cd7795f35c35898b5a6969a2d0ba8e
van <vmyers@olin.edu>
2021-10-31 23:11:14 -0400

    merge upstream

--- 02_etch_a_sketch/rgb.sv
+++ 02_etch_a_sketch/rgb.sv
@@ -1,14 +1,38 @@
-module rgb(rst, button, out);
-
-parameter T = 4;
-
-input wire rst, button;
+module rgb(clk, rst, button, out);
+// advance between colors of the LED on button press, reset to red.
+parameter T = 10;
+input wire clk, rst, button;
 output logic [2:0] out;
 
+enum logic [2:0] {
+  RED = 3'b100,
+  GREEN= 3'b010,
+  BLUE = 3'b001
+} state;
 
-debounce #(.BOUNCE_TICKS(T)) clean(
-  .clk(clk), .rst(rst), .bouncy_in(), .debounced_out())
+wire debounced_out;
+debouncer #(.BOUNCE_TICKS(T)) clean(
+  .clk(clk), .rst(rst), .bouncy_in(button), .debounced_out(debounced_out));
 
-always_comb begin : state_logic
-  count_done = &(counter);
+
+always_ff @(posedge clk) begin : fsm_logic
+  if (rst) begin
+    state <= RED;
+  end else begin
+    case(state)
+      RED : begin
+        state <= GREEN;
+      end
+      GREEN : begin
+        state <= BLUE;
+      end
+      BLUE : begin
+        state <= RED;
+      end
+      default : state <= RED;
+    endcase
+  end
 end
+
+always_comb out = state;
+endmodule
